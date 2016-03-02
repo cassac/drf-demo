408fdba65b5d9091c273ee052ef2e8441ad49807
cassac <hicassac@gmail.com>
2016-03-02 09:12:01 -0800

    adds fortune view get and post in generic view format

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,28 +2,25 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Fortune
 
-class SingleFortuneSerializer(serializers.ModelSerializer):
+# class SingleFortuneSerializer(serializers.ModelSerializer):
 
-	class Meta:
-		model = Fortune
-		fields = ('id', 'content',)
-		# extra_kwargs = {
-		# 	'url': {'view_name': 'fortune-detail', 'lookup_field': 'user_id'},
-		# }	
+# 	class Meta:
+# 		model = Fortune
+# 		fields = ('id', 'content',)
+# 		# extra_kwargs = {
+# 		# 	'url': {'view_name': 'fortune-detail', 'lookup_field': 'user_id'},
+# 		# }	
 
 class FortuneSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Fortune
-		fields = ('url', 'id', 'content',)
-		extra_kwargs = {
-			'url': {'view_name': 'fortune-list', 'lookup_field': 'user_id'},
-		}		
+		fields = ('id', 'content',)
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-	fortunes = FortuneSerializer(many=True)
+	# fortunes = FortuneSerializer(many=True)
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'is_staff', 'fortunes',)
+		fields = ('username', 'email', 'is_staff')#, 'fortunes',)
--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,22 @@
 from django.contrib.auth.models import User
-from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.response import Response
-from .serializers import UserSerializer, SingleFortuneSerializer, FortuneSerializer
+from .serializers import UserSerializer, FortuneSerializer
 
 from .models import Fortune
 
-class UserList(APIView):
-	def get(self, request, user_id=None):
-		context = {'request': request}
-		users = User.objects.all()
-		serializer = UserSerializer(users, many=True, context=context)
+class UserList(generics.ListCreateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class FortuneList(generics.ListCreateAPIView):
+	queryset = Fortune.objects.all()
+	serializer_class = FortuneSerializer
+
+	def list(self, request, user_id):
+		queryset = Fortune.objects.filter(user__id=user_id)
+		serializer = FortuneSerializer(queryset, many=True)
 		return Response(serializer.data)
 
-class UserDetail(APIView):
-	def get(self, request, user_id=None):
-		context = {'request': request}
-		user = User.objects.filter(id=user_id)
-		serializer = UserSerializer(user, many=True, context=context)
-		return Response(serializer.data)
-
-
-class FortuneView(APIView):
-	def get(self, request, user_id):
-		context = {'request': request}
-		fortunes = Fortune.objects.filter(user__id=user_id)
-		serializer = FortuneSerializer(fortunes, many=True, context=context)
-		return Response(serializer.data)
-	
-	def post(self, request):
-		pass
-
-
-class FortuneDetailView(APIView):
-	def get(self, request, user_id, fortune_id):
-		fortune = Fortune.objects.filter(pk=fortune_id).filter(user__id=user_id)
-		serializer = SingleFortuneSerializer(fortune, many=True)
-		return Response(serializer.data)
+	def perform_create(self, serializer):
+		serializer.save(user_id=self.kwargs['user_id'])
