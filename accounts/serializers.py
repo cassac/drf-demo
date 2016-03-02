9204439e51a57cb44e23b34b6edf8c8fa663f1d6
cassac <hicassac@gmail.com>
2016-03-02 11:04:26 -0800

    adds urls for user fortune list

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,8 +11,12 @@ class FortuneSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 	fortunes = FortuneSerializer(many=True, read_only=True)
+	fortune_list = serializers.HyperlinkedIdentityField(
+        view_name='fortune-list',
+        lookup_url_kwarg='user_id'
+    )
 
 	class Meta:
 		model = User
-		fields = ('url', 'username', 'email', 'is_staff', 'fortunes')
+		fields = ('url', 'fortune_list', 'username', 'email', 'is_staff', 'fortunes')
 		extra_kwargs = {'url': {'lookup_url_kwarg': 'user_id'}}
